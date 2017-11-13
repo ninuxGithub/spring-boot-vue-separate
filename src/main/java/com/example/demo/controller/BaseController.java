622a5ccc7329f38136ccc10b84659ba0d7eb4663
ninuxGithub <1732768613@qq.com>
2017-11-13 14:48:47 +0800

    m

--- src/main/java/com/example/demo/controller/BaseController.java
+++ src/main/java/com/example/demo/controller/BaseController.java
@@ -19,7 +19,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Controller;
 
